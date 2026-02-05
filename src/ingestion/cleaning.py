a03be64d5c502817693fd2755079c95d4845ac01
Kailas-Thonnithodi <kthonnithodi@gmail.com>
2026-02-05 13:46:26 +1100

    removed all null rows

--- src/ingestion/cleaning.py
+++ src/ingestion/cleaning.py
@@ -3,5 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-raw_data = r"data/xlsx/raw_data.xlsx"
+raw_data = "data/raw_data/raw_data.xlsx"
 data = pd.read_excel(raw_data)
+print(data)
+
+# check for null value
+data.info()
