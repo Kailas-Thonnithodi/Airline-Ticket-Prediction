89a9e2f89c149ab595696fcab58c254d57638114
Kailas-Thonnithodi <kthonnithodi@gmail.com>
2026-02-05 16:46:01 +1100

    duration is split into hours and minutes and saved as new columns

--- src/ingestion/cleaning/cleaning.py
+++ src/ingestion/cleaning/cleaning.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from warnings import filterwarnings
 
 def change_column_to_datetime(column):
@@ -26,6 +24,17 @@ def hour_minute(data, column):
         data = data.drop(columns=[column])
     return data
 
+def duration_modifier(duration):
+    '''
+    * If duration doesn't have hour, then add 0h (start)
+    * If duration doesn't have minutes, then add 0m (end)
+    '''
+    if 'h' not in duration:
+        duration = '0h' + ' ' + duration
+    if 'm' not in duration:
+        duration = duration + ' ' + '0m'
+    return duration
+
 if __name__ == "__main__":
 
     # Loading Data
@@ -43,6 +52,12 @@ if __name__ == "__main__":
     data = day_month_year(data,"Date_of_Journey")
     data = hour_minute(data, "Dep_Time")
     data = hour_minute(data, "Arrival_Time")
+
+    # For each row in column (x) apply the function lambda.
+    data['Duration'] = data['Duration'].apply(duration_modifier)
+    data['Duration_Hour'] = data['Duration'].apply(lambda duration: int(duration.split(' ')[0][0:-1]))
+    data['Duration_Minutes'] = data['Duration'].apply(lambda duration: int(duration.split(' ')[1][0:-1]))
+    data = data.drop(columns=['Duration'])
 
     data.to_csv('data/cleaned_data/cleaned_data.csv', index=False)
     print("Generated a CSV File in cleaned_data folder!")
