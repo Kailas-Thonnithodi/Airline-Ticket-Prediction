de95e4d576c429e78473bebce89c1e6d47d0c07e
Kailas-Thonnithodi <kthonnithodi@gmail.com>
2026-02-06 23:17:44 +1100

    created guided encoding and label encoding algorithms

---
+++ src/feature_engineering/engineer.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from warnings import filterwarnings
+
+def target_guided_encoding(data: pd.DataFrame, column: str): 
+    sorted_mean_column = data.groupby([column])['Price'].mean().sort_values().index
+    key_pair_column = {key: index for index, key in enumerate(sorted_mean_column, 0)}
+    data[column] = data[column].map(key_pair_column)
+
+def label_encoding(data, column):
+    unique_values = list(data[column].unique())
+    mapping = {category: i for i, category in enumerate(unique_values,0)}
+    data[column] = data[column].map(mapping)
+
+if __name__ == "__main__":
+
+    # Loading Data
+    filterwarnings("ignore")
+    raw_data = "data/raw_data/raw_data.xlsx"
+    data = pd.read_excel(raw_data)
+
+    # Changing New-Delhi instances to Delhi
+    data['Destination'].replace('New Delhi','Delhi',inplace=True)
+
+    # Applying targeted guided encoding on the following columns
+    target_guided_encoding(data, 'Airline')
+    target_guided_encoding(data, 'Source')
+    target_guided_encoding(data, 'Destination')
+
+    # Applying label encoding on the following column
+    label_encoding(data, 'Total_Stops')
